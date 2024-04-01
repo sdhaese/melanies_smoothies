52ff1a63142c22bfc1fcd95644b6837eb465dc07
sdhaese <80075056+sdhaese@users.noreply.github.com>
2024-04-01 14:01:35 -0600

    Update streamlit_app.py
    adding api response for all fruits in table
    Not in Snowflake docs, but set 'use_container_width' to False to eliminate shuttering

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -33,6 +34,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=False)
     
     # st.write(ingredients_string)
 
@@ -49,8 +52,6 @@ if ingredients_list:
         # st.success('Your Smoothie is ordered!', icon="✅")
         st.success(successStr, icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
 
